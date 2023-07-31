4999c07802f78184fde113dd174943e9815c9faf
Bruno Mateus <brunomateus2017@hotmail.com>
2023-07-31 09:57:18 -0300

    First commit

---
+++ src/app/studencrud/studencrud.component.ts
@@ -0,0 +1,78 @@
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-studencrud',
+	templateUrl: './studencrud.component.html',
+	styleUrls: ['./studencrud.component.css']
+})
+export class StudencrudComponent {
+	StudentArray : any[] = [];
+	isResultLoaded = false;
+	isUpdateFormActive = false;
+	
+	stname: string = "";
+	course: string = "";
+	fee: string = "";
+	currentStudentID: string = "";
+
+	constructor(private http: HttpClient){
+		this.getAllStudent();
+	}
+	// ,{'headers':_httpOptions}
+
+	getAllStudent(){
+	
+		this.http.get("http://localhost:8085/api/student").subscribe((resultData:any)=>{
+			console.log(123)
+			this.isResultLoaded = true;
+			this.StudentArray = resultData.data;
+		})
+	}
+
+	register(){
+		let bodyData = {
+			"stname":this.stname,
+			"course":this.course,
+			"fee":this.fee
+		};
+		this.http.put("http://localhost:8085/api/student/add/",bodyData).subscribe((resultData:any)=>{
+			alert("Registered")
+			this.getAllStudent();
+		})
+	}
+
+	updateRecords(){
+		let bodyData = {
+			"stname":this.stname,
+			"course":this.course,
+			"fee":this.fee
+		};
+		this.http.put("http://localhost:8085/api/student/update/"+this.currentStudentID,bodyData).subscribe((resultData:any)=>{
+			alert("Updatedd")
+			this.getAllStudent();
+		})
+	}
+
+	save(){
+		if(this.currentStudentID == ''){
+			this.register();
+		}else{
+			this.updateRecords();
+		}
+	}
+
+	setUpdate(data:any){
+		this.stname = data.stname
+		this.course = data.course
+		this.fee = data.fee
+		this.currentStudentID = data.id;
+	}
+
+	setDelete(data:any){
+		this.http.delete("http://localhost:8085/api/student/add/"+data.id).subscribe((resultData:any)=>{
+			alert("Registered")
+			this.getAllStudent();
+		})
+	}
+}
